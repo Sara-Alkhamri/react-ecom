0f62d3aa994a9d272d233553d2062ec3795fb8ad
Sara Alkhamri <s.alkhamri@gmail.com>
2021-03-04 14:38:55 -0800

    Updates

---
+++ precache-manifest.a7b780f00dc738055ecc477958af3ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf1322204808ef67592f4deea6fa18c",
+    "url": "/react-ecom/index.html"
+  },
+  {
+    "revision": "b81f64c48df56e690eb9",
+    "url": "/react-ecom/static/css/main.7b1dbde0.chunk.css"
+  },
+  {
+    "revision": "f6fc875bbaa0bec49122",
+    "url": "/react-ecom/static/js/2.c506e044.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-ecom/static/js/2.c506e044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81f64c48df56e690eb9",
+    "url": "/react-ecom/static/js/main.48a35a60.chunk.js"
+  },
+  {
+    "revision": "8670f07746580123ff48",
+    "url": "/react-ecom/static/js/runtime-main.a5518043.js"
+  }
+]);
